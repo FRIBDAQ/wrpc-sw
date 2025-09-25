7a4bd646ca6c3ccb597fd3de34b5d423aaa50501
Tristan Gingold <tristan.gingold@cern.ch>
2025-09-25 10:05:21 +0200

    Extract cmd_w1_eeprom.c and cmd_w1_temp.c from w1-shell.c
    And move to shell/ directory

--- dev/dev.mk
+++ dev/dev.mk
@@ -48,7 +48,7 @@ obj-$(CONFIG_TARGET_WR_SWITCH) += dev/timer-wrs.o dev/gpio.o
 obj-$(CONFIG_PUTS_SYSLOG) += dev/puts-syslog.o
 
 obj-$(CONFIG_DAC_LOG) += dev/dac_log.o
-obj-$(CONFIG_W1) +=		dev/w1.o	dev/w1-hw.o	dev/w1-shell.o
+obj-$(CONFIG_W1) +=		dev/w1.o	dev/w1-hw.o
 obj-$(CONFIG_W1_EEPROM) +=	dev/w1-eeprom.o
 obj-$(CONFIG_W1_TEMP) +=	dev/temp-w1.o dev/w1-temp.o
 
---
+++ shell/cmd_w1_temp.c
@@ -0,0 +1,30 @@
+/*
+ * Onewire generic interface
+ * Alessandro Rubini, 2013 GNU GPL2 or later
+ */
+#include "wrc.h"
+#include "shell.h"
+#include "dev/w1.h"
+#include "cmds.h"
+
+#define BLEN 32
+/* A shell command, for checking */
+int cmd_w1(const char *args[])
+{
+	int i;
+	struct w1_dev *d;
+	int32_t temp;
+
+	for (i = 0; i < W1_MAX_DEVICES; i++) {
+		d = wrpc_w1_bus.devs + i;
+		if (d->rom) {
+			pp_printf("device %i: %08x%08x\n", i,
+				  (int)(d->rom >> 32), (int)d->rom);
+		temp = w1_read_temp(d, 0);
+		pp_printf("temp: %d.%04d\n", (int) (temp >> 16),
+			  (int)((temp & 0xffff) * 10 * 1000 >> 16));
+		}
+	}
+	return 0;
+}
+
